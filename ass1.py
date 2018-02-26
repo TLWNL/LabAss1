51fbd5ef56d67bc657991eea3a0a106a479c2b60
tlwnl <dylanrijnsburger@gmail.com>
2018-02-26 10:44:35 +0100

    Added split frames, not done yet.

--- ass1.py
+++ ass1.py
@@ -1,5 +1,5 @@
 line1 = "004b"
-line2 = "004b0049000000275468657265206973206e6f2077617220696e2042612053696e672053652e07"
+line2 = "004b0049000000365468697320746561206973206e6f7468696e67206d6f7265207468616e20686f74206c656166206a756963652161004b00490000005053686172696e6720746561207769746820612066617363696e6174696e6720737472616e676572206973206f6e65206f66206c696665277320747275652064656c69676874732e43" #"004b0049000000275468657265206973206e6f2077617220696e2042612053696e672053652e07"
 
 dest_address = ""
 source_address = ""
@@ -7,6 +7,25 @@ total_frame_length = ""
 payload = ""
 checksum = ""
 rest = ""
+
+def split_frames(line2):
+    frame_length = ""
+    current_start_point = 0
+    current_frame = ""
+
+    for x in range(8, 16):
+        frame_length += line2[x]
+    frame_length_dec = int(frame_length, 16)
+    print(frame_length_dec)
+
+    for x in range(current_start_point, frame_length_dec):
+        current_frame += line2[x]
+    #ALP(current_frame)
+    current_start_point = frame_length_dec
+    for x in range(frame_length_dec + 8, frame_length_dec + 16):
+        frame_length += line2[x]
+    frame_length_dec = int(frame_length, 16)
+
 
 def ALP(line2):
     x = ""
@@ -18,16 +37,25 @@ def ALP(line2):
 
     for z in range(0, len(line2)- 2):
         x += line2[z]
-        #print(x)
         #if there are two bits in x:
         if z % 2 == 1:
             #add the integer version of the hex to total
             total += int(x, 16)
             #reset x
+            print(x)
             x = ""
     print(total)
     calculated_checksum = hex(total % 128)
     print(calculated_checksum)
+
+    if dest_address != line1:
+        print("ADDRESS MISMATCH")
+
+    if calculated_checksum != checksum:
+        print("ERROR")
+
+
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 
 #select frame is not finished
 def select_frame():
@@ -51,15 +79,8 @@ for x in range(8, 16):
 for x in range(16, len(line2)):
     rest += line2[x]
 
-amount_of_frames = len(rest) / 62
+amount_of_frames = len(rest)-2 / 60
 
 ALP(line2)
 
-
-
-#print(dest_address)
-#print(source_address)
-#print(total_frame_length)
-#print(len(line2))
-#print(rest)
 
